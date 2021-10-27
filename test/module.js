2d8c9538aa02d6a70ec5265dee91bc73dbfb492b
Richard Schloss <richard.e.schloss@gmail.com>
2021-10-27 01:06:34 -0400

    Updated module test

--- test/module.js
+++ test/module.js
@@ -3,20 +3,19 @@ import test from 'ava'
 import Module from '../lib/module.js'
 
 test('Module', (t) => {
-  const _modsAdded = []
-  const _tmplsAdded = []
   const opts = { extra: 111 }
   const ctx = {
     Module,
     options: {
+      build: {
+        templates: []
+      },
+      plugins: [],
       modules: [],
       routerUI: 123
     },
-    addModule (mod) {
-      _modsAdded.push(mod)
-    },
-    addTemplate (tmpl) {
-      _tmplsAdded.push(tmpl)
+    addTemplate ({ src, fileName: dst }) {
+      ctx.options.build.templates.push({ src, dst })
     },
     addPlugin ({
       ssr,
@@ -24,6 +23,7 @@ test('Module', (t) => {
       fileName,
       options
     }) {
+      ctx.options.plugins.push({ ssr, src, fileName, options })
       t.false(ssr)
       t.is(src, pResolve('./lib/plugin.js'))
       t.is(fileName, 'nuxt-router-ui/plugin.js')
@@ -35,4 +35,6 @@ test('Module', (t) => {
   }
 
   ctx.Module(opts)
+  t.true(ctx.options.build.templates.length > 0)
+  t.true(ctx.options.plugins.length > 0)
 })
