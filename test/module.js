df61d1d23f4444358dcd7c330e871b4491dfe1d0
Richard Schloss <richard.e.schloss@gmail.com>
2021-10-29 18:42:36 -0400

    Updated tests for module and plugin

--- test/module.js
+++ test/module.js
@@ -1,40 +1,27 @@
-import { resolve as pResolve } from 'path'
+import { resolve } from 'path'
 import test from 'ava'
 import Module from '../lib/module.js'
 
-test('Module', (t) => {
-  const opts = { extra: 111 }
-  const ctx = {
-    Module,
-    options: {
-      build: {
-        templates: []
-      },
-      plugins: [],
-      modules: [],
-      routerUI: 123
-    },
-    addTemplate ({ src, fileName: dst }) {
-      ctx.options.build.templates.push({ src, dst })
-    },
-    addPlugin ({
-      ssr,
-      src,
-      fileName,
-      options
-    }) {
-      ctx.options.plugins.push({ ssr, src, fileName, options })
-      t.false(ssr)
-      t.is(src, pResolve('./lib/plugin.js'))
-      t.is(fileName, 'nuxt-router-ui/plugin.js')
-      t.is(
-        JSON.stringify(options),
-        JSON.stringify({ ...ctx.options.routerUI, ...opts })
-      )
-    }
-  }
+global.__dirname = '/path/to/lib'
 
-  ctx.Module(opts)
-  t.true(ctx.options.build.templates.length > 0)
-  t.true(ctx.options.plugins.length > 0)
+test('Module: adds plugin', (t) => {
+  const ctx = {
+    nuxt: {
+      version: '3.0',
+      hook () {},
+      options: {
+        plugins: []
+      },
+      /**
+       * @param {any} plugin
+       */
+      addPlugin (plugin) {
+        this.options.plugins.push(plugin)
+      }
+    },
+    Module
+  }
+  ctx.Module({})
+  const plugin0 = ctx.nuxt.options.plugins[0]
+  t.is(plugin0.src, resolve(__dirname, 'plugin.js'))
 })
