dba477d9488039351c0601bbc765c1e98cd80794
Richard Schloss <richard.e.schloss@gmail.com>
2021-12-26 21:51:38 -0500

    Updated module test

--- test/module.js
+++ test/module.js
@@ -1,18 +1,13 @@
 import { resolve } from 'path'
 import test from 'ava'
 import Module from '../lib/module.js'
-import { wrapModule } from './utils/module.js'
+import { initNuxt, useNuxt } from './utils/module.js'
 
 global.__dirname = '/path/to/lib'
 
-const delay = ms => new Promise(resolve =>
-  setTimeout(resolve, ms)
-)
-
 test('Module: adds plugin', async (t) => {
-  const ctx = wrapModule(Module)
-  ctx.Module({})
-  await delay(100)
-  const plugin0 = ctx.nuxt.options.plugins[0]
+  initNuxt()
+  await Module({}, useNuxt())
+  const plugin0 = useNuxt().options.plugins[0]
   t.is(plugin0.src, resolve(__dirname, 'plugin.js'))
 })
