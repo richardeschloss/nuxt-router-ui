5966a4f80e2b3dee21f8e04ac8223c15ed6f52db
Richard Schloss <richard.e.schloss@gmail.com>
2022-01-11 14:35:49 -0500

    Updated nuxt3 and module test

--- test/module.js
+++ test/module.js
@@ -1,27 +1,15 @@
 import { resolve } from 'path'
 import test from 'ava'
 import Module from '../lib/module.js'
+import { initNuxt, useNuxt } from './utils/module.js'
 
 global.__dirname = '/path/to/lib'
 
-test('Module: adds plugin', (t) => {
-  const ctx = {
-    nuxt: {
-      version: '3.0',
-      hook () {},
-      options: {
-        plugins: []
-      },
-      /**
-       * @param {any} plugin
-       */
-      addPlugin (plugin) {
-        this.options.plugins.push(plugin)
-      }
-    },
-    Module
-  }
-  ctx.Module({})
-  const plugin0 = ctx.nuxt.options.plugins[0]
-  t.is(plugin0.src, resolve(__dirname, 'plugin.js'))
+test('Module: adds plugin', async (t) => {
+  initNuxt()
+  await Module({}, useNuxt())
+  const nuxt = useNuxt()
+  const [plugin] = nuxt.options.plugins
+  t.is(plugin.src, resolve(__dirname, 'plugin.js'))
+  t.is(nuxt.options.build.transpile[0], __dirname)
 })
---
+++ test/utils/module.js
@@ -0,0 +1,35 @@
+import path from 'path'
+import { nuxtCtx, useNuxt } from '@nuxt/kit'
+
+const srcDir = path.resolve('.')
+
+export { useNuxt }
+
+export function initNuxt () {
+  nuxtCtx.unset()
+  const nuxt = {
+    version: '3.x',
+    hooks: {
+      addHooks: (hooks) => {
+        Object.assign(useNuxt(), hooks)
+      }
+    },
+    hook (evt, cb) {
+      nuxtCtx.use().hooks[evt] = cb
+    },
+    options: {
+      css: [],
+      srcDir,
+      plugins: [],
+      modules: [],
+      serverMiddleware: [],
+      build: {
+        transpile: [],
+        templates: []
+      },
+      publicRuntimeConfig: {}
+    }
+  }
+  // @ts-ignore
+  nuxtCtx.set(nuxt)
+}
